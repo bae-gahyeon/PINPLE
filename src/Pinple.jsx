734766280a10f4e7f9b39ca120c806314f71d165
baegahyeon <qork1203@gmail.com>
2026-07-19 12:43:24 +0900

    타임라인 전용 컴포넌트 TimelineTab으로 격리

--- src/Pinple.jsx
+++ src/Pinple.jsx
@@ -12,7 +12,7 @@ import firebase from "firebase/compat/app";
 import { db } from "./components/firebase";
 
 import MapTab from "./components/MapTab";
-// import TimelineTab from "./components/TimelineTab"; 아직 생성x
+import TimelineTab from "./components/TimelineTab"; 
 // import CalendarTab from "./components/CalendarTab";
 import DashboardTab from "./components/DashboardTab";
 // import RecordModal from "./components/RecordModal";
@@ -113,39 +113,9 @@ export default function Pinple({ currentProfile, setProfile }) {
 
       {/* ⏳ 2. 타임라인 탭 */}
       {activeTab === "timeline" && (
-        <div
-          style={{
-            padding: "20px 20px 85px 20px",
-            overflowY: "auto",
-            height: "100%",
-            boxSizing: "border-box",
-            background: "#f9f9f9",
-          }}
-        >
-          <h2 style={{ borderBottom: "2px solid #e50914", paddingBottom: 10 }}>
-            내 기록 타임라인
-          </h2>
-          {savedRecords.map((r) => (
-            <div
-              key={r.id}
-              style={{
-                marginBottom: 15,
-                padding: 10,
-                background: "white",
-                borderRadius: 8,
-                boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
-              }}
-            >
-              <div style={{ fontSize: 12, color: "gray" }}>{r.date}</div>
-              <div style={{ fontWeight: "bold", fontSize: 16 }}>
-                {r.placeName}
-              </div>
-              <br />
-              <div>💰 {r.cost}원</div>
-              <div style={{ color: "#555", marginTop: 5 }}>📝 {r.memo}</div>
-            </div>
-          ))}
-        </div>
+        <TimelineTab
+        savedRecords={savedRecords}
+        />
       )}
 
       {/* 📅 3. 캘린더 탭 */}
